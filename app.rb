f8c403642e9adde0a8ed7368765096ab16fac293
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-07 09:41:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   
   get "/say/:number/:phrase" do
     @product = params[:phrase] * params[:number].to_i 
-    #"#{@product}"
+    "Super duper #{@product}"
   end
 
 end
