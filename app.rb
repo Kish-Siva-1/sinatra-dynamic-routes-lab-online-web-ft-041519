ae498048c860f99e439fbe2f3b39a4c892564dff
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-07 10:00:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,9 +18,10 @@ class App < Sinatra::Base
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     binding.pry
     @store = []
-    params.each do |x|
-      @store << [x.]
+    params.each do |x,k|
+      @store << [k]
     end 
+    binding.pry
   end
 
 end
