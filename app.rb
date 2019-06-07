feb6c88400264f6cd6f976e7c09a77342e9e5e73
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-07 09:29:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get 'reversename/:name' do 
-    binding.pry
-  end 
+  get "/reversename/:name" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
+  end
 
 end
