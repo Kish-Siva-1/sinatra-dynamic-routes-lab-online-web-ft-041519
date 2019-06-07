2c0d3b33fa0ad4a7c22c787f6e0c535a05c2953a
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-07 10:21:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,8 +25,8 @@ class App < Sinatra::Base
 
   get "/:operation/:number1/:number2" do
     operation = params[:operation]
-    number1 = params[:number1]
-    number2 = params[:number2]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
     
     if operation == "add"
       answer = number1 + number2 
@@ -38,7 +38,6 @@ class App < Sinatra::Base
       answer = number1 / number2 
     end 
     
-    binding.pry
     answer 
     
   end 
